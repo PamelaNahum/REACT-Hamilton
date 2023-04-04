a7baee78f5e2353fe9f8549a4ec06f9574829c9f
PamelaNahum <pamela.nahum14@gmail.com>
2023-04-04 11:57:41 -0400

    servicios

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,28 +1,42 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { FormularioAlumno, TablaAlumno } from "../components";
-import { getAll } from "../services/EstudianteService";
+import { getAll, estudianteAdd, estudianteEdit, estudianteDelete } from "../services/EstudianteService";
 import { Link } from "react-router-dom";
 
 
 const AdminPage = () => {
-    const [estudiante, setEstudiante]=useState(null);
+    const [estudiante, setEstudiante]=useState([]);
 
     const obtenerEstudiantes=async()=>{
         setEstudiante(await getAll());
     }
+
+    const agregarEstudiante = async ()=>{
+        await estudianteAdd();
+    }
+
+    const editarEstudiante = async ()=>{
+        await estudianteEdit();
+    }
+
+    const eliminarEstudiante = async ()=>{
+        await estudianteDelete();
+    }
     //cada vez que la pagina se recargue, el useEffect corre
     //cada vez que las variables que enten dentro de los [] CAMBIEN el useEffect va a correr 
     useEffect(()=>{
-        console.log(estudiante)
         obtenerEstudiantes();
+        agregarEstudiante();
+        //editarEstudiante();
+        //eliminarEstudiante();
     },[])
 
     //esto es un comentario
     return (
         <div className='container md-4'>
             <div className='row'>
-                <TablaAlumno estudiante={estudiante} />
+            <TablaAlumno estudiante={estudiante} />
                 <FormularioAlumno />
                 <Link to="/FormularioDeAlumnos"><button type="button" class="btn btn-info">Ir a formulario</button></Link>
                 <a href="/TablaDeAlumnos"><button type="button" class="btn btn-info">Ir a Tabla</button></a>
--- src/services/EstudianteService.js
+++ src/services/EstudianteService.js
@@ -8,4 +8,58 @@ const getAll = async ()=>{
     return rest.data;
 }
 
-export {getAll}
+//sintaxis de servicio para enviar un estudiante
+const estudianteAdd = async (estudiante) => {
+    /*
+    como debería venir la variable estudiante
+    {
+        estudiante_id:1,
+        nombre: "Pamela",
+        apellido: "Nahum"
+        equipo_id: 1
+    }
+    cuando AGREGAMOS un nuevo estudiante a la base, no debemos preocupar de que no venga el dato id,
+    ya que este se agrega de fomra automatica en la base de datos
+    entonces deberia venir así:
+    {
+        nombre: "Pamela",
+        apellido: "Nahum"
+        equipo_id: 1
+    }
+    
+    */
+    const estu ={
+        nombre: "Pamela",
+        apellido: "Nahum",
+        equipo_id: 5
+    }
+
+   
+    const rest = await axios.post(baseURL+"/estudiante/Save", estu);
+    console.log(rest);
+    return rest.data;
+}
+
+const estudianteEdit =  async (estudiante) =>{
+    /*const estu ={
+        estudiante_id:46,
+        nombre: "Pamela",
+        apellido: "Nahum",
+        equipo_id: 5
+    }*/
+    const rest = await axios.put(baseURL+"/estudiante/Update", estudiante);
+    console.log(rest);
+    return rest.data;
+}
+
+const estudianteDelete = async (id) =>{
+    const estudiante_id= 46
+    //si consideramos que id = 3 -> http://localhost:8080/estudiante/Delete/3
+    const rest = await axios.delete(baseURL+"/estudiante/Delete/"+estudiante_id);
+    console.log(rest);
+    return rest.data;
+}
+
+
+
+export {getAll, estudianteAdd, estudianteEdit, estudianteDelete}
